316f79e9094da48e51ccc45940e83df54e87bcaa
Sandra13988 <sandra13988@gmail.com>
2025-01-20 13:01:35 +0100

    Diseño de landing primer paso hecho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobremiComponent } from './sobremi/sobremi.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { LandingComponent } from "./landing/landing.component";
+
+const appRoutes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'sobre-mi', component: SobremiComponent },
+  { path: 'proyectos', component: ProyectosComponent },
+  { path: 'experiencia', component: ExperienciaComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' }, // Ruta por defecto
+];
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,  LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
-  title = 'Portfolio';
+  title = 'portfolio';
 }
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { InicioComponent } from '../inicio/inicio.component';
+import { SobremiComponent } from '../sobremi/sobremi.component';
+import { ProyectosComponent } from '../proyectos/proyectos.component';
+import { ExperienciaComponent } from '../experiencia/experiencia.component';
+import { ContactoComponent } from '../contacto/contacto.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [InicioComponent, SobremiComponent, ProyectosComponent, ExperienciaComponent, ContactoComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
