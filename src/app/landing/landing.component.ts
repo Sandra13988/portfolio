ce21e9b7cad0f9a27e29693aef0b46096bc62237
Sandra13988 <sandra13988@gmail.com>
2025-01-30 18:03:27 +0100

    Componente habilidades añadido a landing

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -4,11 +4,12 @@ import { SobremiComponent } from '../sobremi/sobremi.component';
 import { ProyectosComponent } from '../proyectos/proyectos.component';
 import { ExperienciaComponent } from '../experiencia/experiencia.component';
 import { ContactoComponent } from '../contacto/contacto.component';
+import { HabilidadesComponent } from "../habilidades/habilidades.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [InicioComponent, SobremiComponent, ProyectosComponent, ExperienciaComponent, ContactoComponent],
+  imports: [InicioComponent, SobremiComponent, ProyectosComponent, ExperienciaComponent, ContactoComponent, HabilidadesComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
