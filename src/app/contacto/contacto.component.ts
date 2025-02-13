6ba2f88323fde103d7b0efa06d331ec3a1f2b43b
Sandra13988 <sandra13988@gmail.com>
2025-02-13 16:41:40 +0100

    agregado boton popup de contacto

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
+  showPopup: boolean = false;
+  email: string = 'sandra13988@gmail.com';
+  copySuccess: string = '';
+
+  togglePopup() {
+    this.showPopup = !this.showPopup;
+  }
+
+  copyToClipboard() {
+    navigator.clipboard.writeText(this.email).then(
+      () => {
+        this.copySuccess = '¡Correo copiado al portapapeles!';
+        setTimeout(() => (this.copySuccess = ''), 3000); // Limpiar mensaje después de 3 segundos
+      },
+      () => {
+        this.copySuccess = 'Error al copiar';
+      }
+    );
+  }
 
 }
