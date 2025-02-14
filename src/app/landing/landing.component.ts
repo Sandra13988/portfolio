8f0938fad2d3842c2d8978d8fe5f7e52aa307f37
Sandra13988 <sandra13988@gmail.com>
2025-02-14 10:45:37 +0100

    Cambiado de lugar el popup de contacto

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -5,14 +5,33 @@ import { ProyectosComponent } from '../proyectos/proyectos.component';
 import { ExperienciaComponent } from '../experiencia/experiencia.component';
 import { ContactoComponent } from '../contacto/contacto.component';
 import { HabilidadesComponent } from "../habilidades/habilidades.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [InicioComponent, SobremiComponent, ProyectosComponent, ExperienciaComponent, ContactoComponent, HabilidadesComponent],
+  imports: [InicioComponent, SobremiComponent, ProyectosComponent, ExperienciaComponent, ContactoComponent, HabilidadesComponent, CommonModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
-  
+  showPopup: boolean = false;
+  email: string = 'sandra13988@gmail.com';
+  copySuccess: string = '';
+
+  togglePopup() {
+    this.showPopup = !this.showPopup;
+  }
+
+  copyToClipboard() {
+    navigator.clipboard.writeText(this.email).then(
+      () => {
+        this.copySuccess = '¡Correo copiado al portapapeles!';
+        setTimeout(() => (this.copySuccess = ''), 3000); // Limpiar mensaje después de 3 segundos
+      },
+      () => {
+        this.copySuccess = 'Error al copiar';
+      }
+    );
+  }
 }
